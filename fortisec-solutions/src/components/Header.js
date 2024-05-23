14e8b7c235c66c83b609e65ddf7a108e17fa97bc
Hiksen <iksenx@hotmail.com>
2024-05-22 22:01:33 -0500

    Version 1.1

--- fortisec-solutions/src/components/Header.js
+++ fortisec-solutions/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>My Website</h1>
+      <h1>FortiSec - Solutions</h1>
       <nav>
         <ul>
           <li><a href="#home">Home</a></li>
